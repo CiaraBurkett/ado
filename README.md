4c874fbcedd5dc80e7f6434ff0493eec8761fffc
Ciara Burkett <ciaraburkett@gmail.com>
2015-11-24 19:28:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ado, a manual todo list
+Learning some escape characters in C.
+
+## resources
+[Unicode Character Bullet](http://www.fileformat.info/info/unicode/char/2022/index.htm)
---
+++ src/main.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  A manual (hand-coded) to-do list
+ *
+ *        Version:  0.0.1
+ *        Created:  11/24/2015 18:34:41
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  ciara 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  puts("Today's TODO List");
+  puts("=================");
+  printf("\u2022 Wash the dishes\n\u2022 Clean living room\n\u2022 Discard papers on kitchen table");
+
+  return 0;
+}
+
